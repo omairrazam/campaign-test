710f9a0e5481d252152c4f618bf6db7fc5289e40
Umair <omsolutionpk@gmail.com>
2019-05-17 23:25:20 +0500

    age displaying in user stats

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,14 +3,14 @@ class HomeController < ApplicationController
 
   def index
   	now = Time.now.utc.to_date
-  	age = now.year - current_user.dob.year
+  	@age = now.year - current_user.dob.year
 
   	q  = {
   		network_in: current_user.my_networks,
   		likes_lteq: current_user.yt_likes_count.to_i, 
   		views_lteq: current_user.yt_views_count.to_i,
-  		from_age_lteq: age,
-  		to_age_gteq: age,
+  		from_age_lteq: @age,
+  		to_age_gteq:   @age,
   		country_eq: current_user.country
   	}
 
