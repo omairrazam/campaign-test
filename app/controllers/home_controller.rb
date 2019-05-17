1e351858b17979c3cfbf2d264da7c8e3095cbc39
Umair <omsolutionpk@gmail.com>
2019-05-17 20:04:38 +0500

    authenticate user only on app controllers and not admin

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
+  before_action :authenticate_user!
+
   def index
   	q  = {network_in: current_user.my_networks,likes_lteq:current_user.yt_likes_count.to_i, views_lteq:current_user.yt_views_count.to_i}
   	@q = HiveCampaign.ransack(q)
